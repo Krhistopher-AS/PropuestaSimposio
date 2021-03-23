71e8f8b6d1897130d7c2395b2b358acbe5a73bf8
Krhistopher <krissiguenza@gmail.com>
2021-03-23 10:11:01 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+// Krhistopher Alexander Sigüenza Piedrasanta - 1517221
+using namespace std;
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
+  cout << "Ingrese a la carrera que pertenese:\n" << endl;
+
   std::cin.ignore();
   return 0;
 }
